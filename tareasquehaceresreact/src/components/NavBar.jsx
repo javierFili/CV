b256417abaab7b5d40b0442f1ccd12fc4dab5333
javierFili <javierfiligranaagreda@gmail.com>
2022-12-15 18:49:16 -0400

    sha casi

--- tareasquehaceresreact/src/components/NavBar.jsx
+++ tareasquehaceresreact/src/components/NavBar.jsx
@@ -1,8 +1,18 @@
-export default function NavBar(){
+import "../styles/navbar.css";
 
+export default function NavBar() {
   return (
-    <div>
-      Aqui esta los titulos XDD 
+    <div className="main">
+      <div className="navbar">
+        <div className="row">
+          <div className="col-md-6">
+            <button className="btn btn-primary">boton</button>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-primary">boton</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
