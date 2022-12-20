bebfaca9767c223f6d51e6ac70c3e8feb9f20001
javierFili <javierfiligranaagreda@gmail.com>
2022-12-20 14:54:13 -0400

    lista la entrada

--- tareasquehaceresreact/src/components/NavBar.jsx
+++ tareasquehaceresreact/src/components/NavBar.jsx
@@ -1,21 +1,46 @@
 import "../styles/navbar.css";
+import { AiFillShopping } from "react-icons/ai";
 
 export default function NavBar() {
-  return (
-    <div className="main">
-      <div className="row justify-content-md-center">
-        <div className="col-3">
-          <button className="btn btn-primary ">Crear tarea </button>
-        </div>
-        <div className="col-6">
-          <h1>
-            <b>TAREAS Y QUE HACERES</b>
-          </h1>
-        </div>
-        <div className="col-3">
-          <button className="btn btn-primary">salir</button>
+  let id = localStorage.getItem("usId");
+  if (id === null) {
+    return (
+      <div className="main">
+        <div className="row justify-content-md-center">
+          <div className="col-3">
+            <AiFillShopping />
+            <button className="btn btn-primary ">Crear tarea </button>
+          </div>
+          <div className="col-6">
+            <h1>
+              <b>TAREAS Y QUE HACERES</b>
+            </h1>
+          </div>
+          <div className="col-3">
+            <button className="btn btn-primary">salir</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="main">
+        <div className="row justify-content-md-center">
+          <div className="col-3">
+            <AiFillShopping />
+            <button className="btn btn-primary ">Crear tarea </button>
+          </div>
+          <div className="col-6">
+            <h1>
+              <b>TAREAS Y QUE HACERES</b>
+            </h1>
+          </div>
+          <div className="col-3">
+            <button className="btn btn-primary">salir</button>
+            <AiFillShopping />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
