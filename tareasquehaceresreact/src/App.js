44663c90c019f6fff7d28900c45faa90af5d26fd
javierFili <javierfiligranaagreda@gmail.com>
2023-01-11 17:50:38 -0400

    agregando registro de usuario

--- tareasquehaceresreact/src/App.js
+++ tareasquehaceresreact/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import VistaPrincipalUs from './pages/vistaPrincipalUsuario/VistaPrincipalUs'
 import VistaInicial from './pages/vistaInicial/VistaInicial'
 import InicioDeSesion from './pages/inicioDeSesion/InicioDeSesion';
+import RegistroDeUsuario from './pages/registroUsuario/RegistroDeUsuario';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Router>
 				<Routes>
 					<Route path='/vistaPrincipal' element={<VistaPrincipalUs />} />		
-          <Route path='/inicioDeSesion' element={<InicioDeSesion />} />		
+          <Route path='/inicioDeSesion' element={<InicioDeSesion />} />	
+          <Route path='/registroUsuario' element={<RegistroDeUsuario />} />	          	
           <Route path='/*' element={<VistaInicial/>} />
 				</Routes>
 			</Router>
--- tareasquehaceresreact/src/pages/inicioDeSesion/InicioDeSesion.jsx
+++ tareasquehaceresreact/src/pages/inicioDeSesion/InicioDeSesion.jsx
@@ -6,53 +6,55 @@ import {
   MDBRow,
   MDBBtn,
   MDBInput,
-  MDBCheckbox,
+  MDBCard,
+  MDBCardBody,
 } from "mdb-react-ui-kit";
 import "../../styles/inicioDeSesion.css";
 
 export default function InicioDeSesion() {
   return (
     <MDBContainer fluid className="p-3 my-5">
-      <MDBRow>
-        <MDBCol col="10" md="6">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-            class="img-fluid"
-            alt="Phone image"
-          />
-        </MDBCol>
+      <MDBCard className="text-black m-5" style={{ borderRadius: "25px" }}>
+        <MDBCardBody>
+          <MDBRow>
+            <MDBCol col="10" md="6">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                class="img-fluid"
+                alt="Phone image"
+              />
+            </MDBCol>
 
-        <MDBCol col="4" md="6">
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Email address"
-            id="formControlLg"
-            type="email"
-            size="lg"
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Password"
-            id="formControlLg"
-            type="password"
-            size="lg"
-          />
+            <MDBCol col="4" md="6">
+              <h4 classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                Inicio de sesion
+              </h4>
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Email address"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Password"
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
 
-          <div className="d-flex justify-content-between mx-4 mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              value=""
-              id="flexCheckDefault"
-              label="Remember me"
-            />
-            <a href="!#">Desea registrarse</a>
-          </div>
+              <div className="d-flex justify-content-between mx-4 mb-4">
+                <a href="/registroUsuario">Desea registrarse</a>
+              </div>
 
-          <MDBBtn className="mb-4 w-100" size="lg">
-            Sign in
-          </MDBBtn>
-        </MDBCol>
-      </MDBRow>
+              <MDBBtn className="mb-4 w-100" size="lg">
+                Sign in
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </MDBCardBody>
+      </MDBCard>
     </MDBContainer>
   );
 }
